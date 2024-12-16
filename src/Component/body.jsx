30ca2050bbd1ed2a9ecb42156f1f7304ff0f2363
pranto-islam <prantoislamnt51@gmail.com>
2024-12-17 02:58:37 +0600

    Add Body and LeftPart components; remove App and HomePage components

---
+++ src/Component/body.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+const Body = () => {
+  return (
+    <dev className="">
+      <h1> sdfkhasd </h1>
+    </dev>
+  );
+};
+
+export default Body;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx';
+// import Body from './Component/body';
+import LeftPart from './Component/left_part'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <Body /> */}
+    <LeftPart />
   </StrictMode>,
 )
